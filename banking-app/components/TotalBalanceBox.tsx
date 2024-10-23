7ed7622bb50f4d8bd75eba8b2e1eb46504f34c98
Soo <parksk971031@gmail.com>
2024-10-23 01:00:30 -0400

    ongoing TotalBalanceBox

---
+++ banking-app/components/TotalBalanceBox.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const TotalBalanceBox = ({accounts, totalBanks, totalCurrentBalance}:TotlaBalanceBoxProps) => {
+  return (
+    <section className='total-balance flex'>
+      <div className="total-balance-chart">
+        {/* chart */}
+      </div>
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">
+          {totalBanks} Bank Accounts
+        </h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">
+            Total Current Balance
+          </p>
+          <p className="total-balance-amount flex-center gap-2">
+            ${totalCurrentBalance}
+          </p>
+        </div>
+      </div>
+      
+    </section>
+  )
+}
+
+export default TotalBalanceBox
