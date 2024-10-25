b0a4b79182f563c8dc68d509a95188bbf8215f8a
Soo <parksk971031@gmail.com>
2024-10-25 00:07:36 -0400

    bug: AuthForm SignUp import doesn't work. doesn't pass to user.actions.ts SignUp Function

--- banking-app/components/AuthForm.tsx
+++ banking-app/components/AuthForm.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -10,9 +11,9 @@ import { Form } from "@/components/ui/form"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react'
-import SignUp from '@/app/(auth)/sign-up/page';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
+import { signUp } from '@/lib/actions/user.actions';
+
 
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -46,8 +47,22 @@ const AuthForm = ({ type }: { type: string }) => {
             }
             // sign up with appwrite & create plaid token link
             if(type === 'sign-up'){
-                // const newUser = await signUp(data)
-                // setUser(newUser)
+                const userData={
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+
+                const newUser = await signUp(userData)
+                
+                setUser(newUser)
             }
         }
         catch(error){
@@ -111,7 +126,7 @@ const AuthForm = ({ type }: { type: string }) => {
                                         <CustomInput control={form.control} name={"postalCode"} label={"Postal Code"} placeholder={"ex: 11101"}/>
                                     </div>
                                     <div className="flex gap-4">
-                                        <CustomInput control={form.control} name={"dob"} label={"Date of Birth"} placeholder={"yyyy-mm-dd"}/>
+                                        <CustomInput control={form.control} name={'dateOfBirth'} label={"Date of Birth"} placeholder={"yyyy-mm-dd"}/>
                                         <CustomInput control={form.control} name={"ssn"} label={"Social Secruity Number"} placeholder={"ex: 1234"}/>
                                     </div>
                                 </>
--- banking-app/lib/actions/user.actions.ts
+++ banking-app/lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 'use server';
 
+import {createSessionClient, createAdminClient} from "../appwrite";
+import { ID } from 'node-appwrite'
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async() => {
     try{
         // Mutation / Database / Fetch
@@ -9,11 +14,42 @@ export const signIn = async() => {
     }
 }
 
-export const signUp = async(userData:SignUpParams) => {
+// export const signUp = async (userData:SignUpParams) => {
+    export const signUp = async (userData:SignUpParams) => {
+    console.log('come here')
+    const {email, password, firstName, lastName} = userData
     try{
-        // Create a user account using appwrite
+        // Create a user account using appwrite - create from appwrite signup with email doc
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+            );
+        const session = await account.createEmailPasswordSession(email, password);
+
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+        return parseStringify(newUserAccount)
     }
     catch(error){
         console.log('Error in SignUp user.actions : ',error)
     }
 }
+
+export async function getLoggedInUser() {
+    try {
+        const { account } = await createSessionClient();
+        return await account.get();
+    } catch (error) {
+        return null;
+    }
+}
+
