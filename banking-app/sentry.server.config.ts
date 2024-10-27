548be84992ccfe2f718a9a3364955a3a8c5d6948
Soo <parksk971031@gmail.com>
2024-10-27 16:55:47 -0400

    succeed sentry implementation

---
+++ banking-app/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cb572a6ca9c379d67670c68fceeee04e@o4508139032674304.ingest.us.sentry.io/4508139036409856",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
